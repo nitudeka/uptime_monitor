4d31c159b45d712f6541a8d8716c31e0a198a66e
nitudeka <nitumonideka327@gmail.com>
2018-11-12 17:09:57 +0530

    added parsing pathname

--- index.js
+++ index.js
@@ -5,10 +5,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
+	// Get the URL and parse it
+	const parsedUrl = url.parse(req.url, true);
+
+	// Get the path from
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+	// Send the response
   res.end('Hello world\n');
+
+  // Log the request path
+  console.log('Request recieved on path: ' + trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
