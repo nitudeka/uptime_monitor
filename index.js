35f3c6ec0fcee531921a32b4b971a6adb4013a10
nitudeka <nitumonideka327@gmail.com>
2018-11-12 17:20:14 +0530

    added query string object

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
-* Primary file for the api
-*
-*/
+ * Primary file for the api
+ *
+ */
 
 // Dependencies
 const http = require('http');
@@ -16,11 +16,18 @@ const server = http.createServer((req, res) => {
 	const path = parsedUrl.pathname;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+	// Get the query string as an object
+	const queryStringObject = parsedUrl.query;
+	console.log(queryStringObject);
+
+	// Get the HTTP method
+	const method = req.method.toLowerCase();
+
 	// Send the response
   res.end('Hello world\n');
 
   // Log the request path
-  console.log('Request recieved on path: ' + trimmedPath);
+  console.log('Request recieved on path ' + trimmedPath + ' with method ' + method);
 });
 
 // Start the server, and have it listen on port 3000
