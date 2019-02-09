7404c826cd596463ecd81a72832cad350f3d8f15
nitudeka <nitumonideka327@gmail.com>
2019-02-09 13:14:27 +0530

    adding functionality to get data from a POST request

--- index.js
+++ index.js
@@ -5,10 +5,31 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // create a http server
 const server = http.createServer((req, res) => {
-	res.end('Hello world\n');
+	// get the URL and parase it
+	const parsedUrl = url.parse(req.url, true);
+
+	// trim the slashes from the pathname
+	parsedUrl.pathname = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+
+	const decoder = new StringDecoder;
+	let data = '';
+
+	// get the payload, if any
+	req.on('data', (chunk) => {
+		data += decoder.write(chunk);
+	});
+	req.on('end', () => {
+		data += decoder.end();
+
+		// send the response
+		res.end('Hello world\n');
+	});
+
 });
 
 // listen on a port for requests
